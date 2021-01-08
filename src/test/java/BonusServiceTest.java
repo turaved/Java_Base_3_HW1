16d988d4fb320c8be1536505c76793d407ce997f
Alex Turovets <madlilu177@gmail.com>
2021-01-08 22:17:51 +0300

    2 tests added

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -27,4 +27,30 @@ class BonusServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @org.junit.jupiter.api.Test //annotation
+    void shouldCalculateUnRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test //annotation
+    void shouldCalculateUnRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_000_60;
+        boolean registered = false;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
 }
